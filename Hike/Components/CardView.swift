7f605176ee3b8190326d53e97280efa3e2950c9d
Harsh Vardhan Singh Shekhawat <hvardhan665@gmail.com>
2024-03-31 09:25:44 +0530

    New Update : Date 31 march 1. MotionAnimationView 2. GradientButtonStyle 3. CustomCircleView

--- Hike/Components/CardView.swift
+++ Hike/Components/CardView.swift
@@ -8,7 +8,23 @@
 import SwiftUI
 
 struct CardView: View {
+    
+    //MARK : - properties
+    
+    @State private var imageNumber : Int = 1
+    @State private var randomNumber : Int = 0
+    
+    //MARK: - FUNCTIONS
+    
+    func randomImage(){
+        repeat {
+        randomNumber = Int.random(in: 1...5)
+        } while randomNumber == imageNumber
+        imageNumber = randomNumber
+    }
+    
     var body: some View {
+        //MARK: - CARD
         ZStack {
             CustomBackgroundView()
             VStack {
@@ -44,12 +60,25 @@ struct CardView: View {
                                 )
                         .frame(width: 256,height: 256)
                     
-                    Image("image-1")
+                    Image("image-\(imageNumber)")
                     .resizable()
                     .scaledToFit()
-                }
-            }
-        }
+                    .animation(.easeOut(duration: 0.5),value: imageNumber)
+                }//:VSTACK
+                
+                Button{
+                    //Action
+                    randomImage()
+                } label: {
+                    Text("Explore More")
+                        .font(.title2)
+                        .fontWeight(.heavy)
+                        .foregroundStyle(LinearGradient(colors: [.greenLight, .greenMedium], startPoint: .top, endPoint: .bottom))
+                        .shadow(color: .black.opacity(0.25), radius: 0.25, x: 1, y: 2)
+                } 
+                .buttonStyle(GradientButton())
+            }//:ZSTACK
+        }//CARD
         .frame(width: 320, height: 570)
     }
 }
---
+++ Hike/Utility/GradientButtonStyle.swift
@@ -0,0 +1,8 @@
+//
+//  GradientButtonStyle.swift
+//  Hike
+//
+//  Created by harsh vardhan singh on 30/03/24.
+//
+
+import Foundation
